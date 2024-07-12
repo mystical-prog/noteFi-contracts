b6cd6c7f57035d1e1017fe507b6e4b8ea091a65f
mystical-prog <jdgt.vd.0405@gmail.com>
2024-07-12 13:52:15 +0530

    added inline comments

--- src/primary/Factory.sol
+++ src/primary/Factory.sol
@@ -5,11 +5,29 @@ import {CallOption} from "./Call.sol";
 import {PutOption} from "./Put.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title OptionsFactory
+ * @author PsyCode Labs
+ *
+ * Contract that handles the logic and implementation of a factory contract which is responsible
+ * for creating new options and keeping a list of all options
+ *
+ */
 contract OptionsFactory is Ownable {
-    address[] public callOptions;
-    address[] public putOptions;
+
+    /* ============ State Variables ============ */
+
+    // address of NOTE
     address public premiumToken;
+    // array to store addresses of all deployed call options
+    address[] public callOptions;
+    // array to store addresses of all deployed put options
+    address[] public putOptions;
+
+    // mapping which stores addresses of price oracles corresponding to token address of an asset token
     mapping(address => address) public priceOracles;
+
+    /* ============ Events ============ */
 
     event CallOptionCreated(
         address indexed optionAddress,
@@ -29,14 +47,35 @@ contract OptionsFactory is Ownable {
         uint256 expiration
     );
 
+    /* ============ Constructor ============ */
+
     constructor(address _premiumToken) Ownable(msg.sender) {
         premiumToken = _premiumToken;
     }
 
+    /* ============ Functions ============ */
+
+    /**
+     * Setting Price Oracle - this function adds/updates the address of price oracles corresponding to
+     *     asset tokens in the priceOracles mapping
+     *
+     * @param _asset - address of an asset token
+     * @param _priceOracle - address of price oracle corresponding to asset token
+     */
     function setPriceOracle(address _asset, address _priceOracle) external onlyOwner {
         priceOracles[_asset] = _priceOracle;
     }
 
+    /**
+     * Creating Call Option - this function creates a new call option and adds the address of this newly
+     *     created option in the callOptions array
+     *
+     * @param _asset - address of asset token
+     * @param _premium - price/premium amount in order to buy the option
+     * @param _strikePrice - price at or above which option becomes in the money
+     * @param _quantity - amount of asset token
+     * @param _expiration - timestamp on which the option expires
+     */
     function createCallOption(
         address _asset,
         uint256 _premium,
@@ -55,6 +94,16 @@ contract OptionsFactory is Ownable {
         emit CallOptionCreated(address(_newCallOption), _asset, _premium, _strikePrice, _quantity, _expiration);
     }
 
+    /**
+     * Creating Put Option - this function creates a new put option and adds the address of this newly
+     *     created option in the putOptions array
+     *
+     * @param _asset - address of asset token
+     * @param _premium - price/premium amount in order to buy the option
+     * @param _strikePrice - price at or above which option becomes in the money
+     * @param _quantity - amount of asset token
+     * @param _expiration - timestamp on which the option expires
+     */
     function createPutOption(
         address _asset,
         uint256 _premium,
@@ -72,10 +121,16 @@ contract OptionsFactory is Ownable {
         emit PutOptionCreated(address(_newPutOption), _asset, _premium, _strikePrice, _quantity, _expiration);
     }
 
+    /**
+     * Call Options Getter - this function returns a complete list (addresses) of call options created till date
+     */
     function getCallOptions() external view returns (address[] memory) {
         return callOptions;
     }
 
+    /**
+     * Put Options Getter - this function returns a complete list (addresses) of put options created till date
+     */
     function getPutOptions() external view returns (address[] memory) {
         return putOptions;
     }
--- src/secondary/Offer.sol
+++ src/secondary/Offer.sol
@@ -4,16 +4,40 @@ pragma solidity ^0.8.0;
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "./OfferInterface.sol";
 
+/**
+ * @title Offer
+ * @author PsyCode Labs
+ *
+ * Contract that handles the logic and implementation of an Offer (reselling of an option), each Offer
+ * corresponds to a single option which can be either call or put
+ *
+ */
 contract Offer {
-    OptionContract public optionContract;
+    
+    /* ============ State Variables ============ */
+
+    // address of the current buyer of an option who wants to resell
     address public seller;
-    bool public executed;
+
+    // amount that the seller wants in NOTE
     uint256 public ask;
+
+    // boolean to indicate whether offer has been executed or not
+    bool public executed;
+
+    // underlying option contract
+    OptionContract public optionContract;
+
+    // quote token (NOTE)
     IERC20 public premiumToken;
+
+    /* ============ Events ============ */
 
     event OfferBought(address indexed optionAddress, uint256 indexed ask, address buyer, address seller);
 
     event OfferCancelled(address indexed optionAddress, uint256 indexed ask);
+
+    /* ============ Constructor ============ */
 
     constructor(address _optionContract, address _seller, address _premiumToken, uint256 _ask) {
         optionContract = OptionContract(_optionContract);
@@ -23,16 +47,32 @@ contract Offer {
         executed = false;
     }
 
+    /* ============ Modifiers ============ */
+
+    /**
+     * Throws if buyer of underlying option contract is not this offer contract, the seller will have
+     *     to transfer the buyer role (of option) to corresponding offer contract
+     */
     modifier isAuthorized() {
         require(optionContract.buyer() == address(this), "The offer contract has not been authorized yet!");
         _;
     }
 
+    /**
+     * Throws if offer is already executed
+     */
     modifier isExecutable() {
         require(executed == false, "The offer contract has already been executed!");
         _;
     }
 
+    /* ============ Functions ============ */
+
+    /**
+     * Accept Offer - this function can be called by any interested buyer who want to accept the offer
+     *     and claim the buyer role of the underlying option, buyer will have to transfer asked
+     *     amount of NOTE
+     */
     function buy() external isAuthorized isExecutable {
         require(msg.sender != seller, "The seller should call the cancel function instead");
         executed = true;
@@ -42,6 +82,10 @@ contract Offer {
         emit OfferBought(address(optionContract), ask, msg.sender, seller);
     }
 
+    /**
+     * Revert Offer - this function can only be called by the seller, this will mark offer as execute
+     *     and contract will transfer the buyer role back to the seller
+     */
     function cancel() external isAuthorized isExecutable {
         require(msg.sender == seller, "Only the seller can call this function!");
         executed = true;
